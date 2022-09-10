c31acf48aad0f278d7dba9fc60e96cd7cb5398fa
Kuba <koperkuba@gmail.com>
2022-09-10 11:34:46 +0200

    starter

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,20 @@
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+javaScriptIsFun = 'YES!';
+console.log(typeof javaScriptIsFun);
+
+let year
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
