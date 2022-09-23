9118c3cd739e2b34e3b901984ddd510eb6d5f891
koperkuba <koperkuba@gmail.com>
2022-09-23 10:44:40 +0200

    New modified

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,21 +1,33 @@
 
-let javaScriptIsFun = true;
-console.log(javaScriptIsFun);
+// let javaScriptIsFun = true;
+// console.log(javaScriptIsFun);
 
-// console.log(typeof true);
-console.log(typeof javaScriptIsFun);
-// console.log(typeof 23);
-// console.log(typeof 'Jonas');
+// // console.log(typeof true);
+// console.log(typeof javaScriptIsFun);
+// // console.log(typeof 23);
+// // console.log(typeof 'Jonas');
 
-javaScriptIsFun = 'YES!';
-console.log(typeof javaScriptIsFun);
+// javaScriptIsFun = 'YES!';
+// console.log(typeof javaScriptIsFun);
 
-let year
-console.log(year);
-console.log(typeof year);
+// let year
+// console.log(year);
+// console.log(typeof year);
 
-year = 1991;
-console.log(typeof year);
+// year = 1991;
+// console.log(typeof year);
 
-console.log(typeof null);
-console.log(typeof year)
+// console.log(typeof null);
+// console.log(typeof year)
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+birthYear = 1990;
+
+var job = "programmer";
+job = "teacher";
+
+lastName = 'Schmiedtmann';
+
